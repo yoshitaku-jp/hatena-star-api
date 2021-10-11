362e1eae0ad6f07475d17331de8ced0bd5f935ee
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-10-11 22:20:30 +0900

    出力する文字列を変更

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import { Star } from "./star";
 const argv = require("minimist")(process.argv.slice(2));
 const chalk = require("chalk");
 
-console.log(argv);
-
 async function main() {
   const client = new Client();
   const body = await client.getStar(argv._);
@@ -13,15 +11,15 @@ async function main() {
 
   if (argv["a"]) {
     const stars = await star.getAllStar(body);
-    console.log(stars);
-  } else if (argv["d"]) {
-    const starsDetail = await star.getAllStarDetail(body);
-    console.log(starsDetail);
+    console.log("You have " + stars + " stars");
   } else if (argv["c"]) {
     const color = argv.c;
     const chalkColor = chalk.keyword(color);
     const colorStars = await star.getColorStar(body, color);
-    console.log(chalkColor(colorStars));
+    console.log("You have " + chalkColor(colorStars) + " " + color + " stars");
+  } else {
+    const stars = await star.getAllStar(body);
+    console.log("You have " + stars + " stars");
   }
 }
 
