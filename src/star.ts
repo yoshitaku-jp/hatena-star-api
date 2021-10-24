0f20b25728d20803aba062269165459833aa54e1
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-10-24 09:14:52 +0900

    変数に入れずreturnを返すように変更

--- src/star.ts
+++ src/star.ts
@@ -3,14 +3,12 @@ import { HatenaStarResponse, Stars } from "./HatenaStarResponse";
 class Star {
   public getAllStar(body: string): number {
     const bodyJson: HatenaStarResponse = JSON.parse(body);
-    const star_count = bodyJson.star_count;
-    return star_count;
+    return bodyJson.star_count;
   }
   public getColorStar(body: string, targetColor: keyof Stars): number {
     const bodyJson: HatenaStarResponse = JSON.parse(body);
     const count: Stars = bodyJson.count;
-    const colorStartCount: number = Number(count[targetColor]);
-    return colorStartCount;
+    return Number(count[targetColor]);
   }
 }
 module.exports = Star;
